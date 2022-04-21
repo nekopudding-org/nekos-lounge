3dd4488a8d05c04ef9f0f766558ae8dc45bf46c5
nekopudding <61680928+nekopudding@users.noreply.github.com>
2022-04-20 19:45:54 -0700

    added campfire gif

--- client/src/components/Campfire/Campfire.jsx
+++ client/src/components/Campfire/Campfire.jsx
@@ -1,8 +1,13 @@
+import { Box } from '@mui/system'
 import React from 'react'
 
 function Campfire() {
   return (
-    <div>Campfire</div>
+    <>
+    <Box sx={{flexGrow: 1, height: 'auto', width: '100%', display: 'flex', justifyContent: 'center', mt: 30, mb: 8.5}}>
+      <Box component='img' src='images/campfire.gif' sx={{height: 500, width: 500}} />
+    </Box>
+    </>
   )
 }
 
